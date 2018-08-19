4953576d91e45cde72a2ee4766243cf6baf98f93
Emmanuel Meinike <emathegreat@gmail.com>
2018-08-19 14:17:58 +0200

    add emoji style for variations

--- src/components/EmojiGrid/EmojiGrid.jsx
+++ src/components/EmojiGrid/EmojiGrid.jsx
@@ -4,7 +4,11 @@ const EmojiGrid = ({ emojis }) => {
     return (
         <div className="emojiGrid">
             {emojis.map(emoji => (
-                <div className="emojiEntry" key={emoji.name}>
+                <div
+                    className={`emojiEntry ${emoji.variations &&
+                        'hasVariations'}`}
+                    key={emoji.name}
+                >
                     <div className="emojiEntry-emoji">{emoji.emoji}</div>
                     <div className="emojiEntry-text">{emoji.name}</div>
                 </div>
