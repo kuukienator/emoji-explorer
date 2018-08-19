35e64bf6176d4016c5cf48a943e268920414db05
Emmanuel Meinike <emathegreat@gmail.com>
2018-08-19 10:37:13 +0200

    add jsdoc to emoji parser

--- emoji-parser.js
+++ emoji-parser.js
@@ -1,24 +1,93 @@
+'use strict';
+
 const https = require('https');
 const fs = require('fs');
 
-const URL = 'https://unicode.org/Public/emoji/11.0/emoji-test.txt';
+/**
+ *
+ * @type {string}
+ */
+const EMOJI_URL = 'https://unicode.org/Public/emoji/11.0/emoji-test.txt';
 
-const getEmojiNameFromLine = line => line.split('#')[1].trim().split(' ').splice(1).join(' ');
+/**
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+const getEmojiNameFromLine = line =>
+    line
+        .split('#')[1]
+        .trim()
+        .split(' ')
+        .splice(1)
+        .join(' ');
+
+/**
+ *
+ * @param {string} line
+ * @returns {string}
+ */
 const getEmojiCodePointFromLine = line => line.split(';')[0].trim();
-const getEmojiFromCodePoins = codepoints => String.fromCodePoint(...(codepoints.split(' ').map(cp => parseInt(cp, 16))));
-const writeFile = (name, data) => new Promise((resolve, reject) => fs.writeFile(name + '.json', JSON.stringify(data), err => err ? reject(err) : resolve()))
+
+/**
+ *
+ * @param {string} codepoints
+ * @returns {string}
+ */
+const getEmojiFromCodePoins = codepoints =>
+    String.fromCodePoint(...codepoints.split(' ').map(cp => parseInt(cp, 16)));
+
+/**
+ *
+ * @param {string} name
+ * @param {Object} data
+ * @returns {Promise}
+ */
+const writeFile = (name, data) =>
+    new Promise((resolve, reject) =>
+        fs.writeFile(
+            name + '.json',
+            JSON.stringify(data),
+            err => (err ? reject(err) : resolve())
+        )
+    );
+
+/**
+ *
+ * @param {string} line
+ * @returns {string}
+ */
 const getGroupNameFromLine = line => line.split(':')[1].trim();
+
+/**
+ *
+ * @param {string} line
+ * @returns {boolean}
+ */
 const filterNonQualifiedEmoji = line => !line.includes('non-fully-qualified');
 
-const getTextFile = url => new Promise((resolve, reject) => {
-    https.get(url, resp => {
-        let data = '';
+/**
+ *
+ * @param {string} url
+ * @returns {Promise<string>}
+ */
+const getTextFile = url =>
+    new Promise((resolve, reject) => {
+        https
+            .get(url, resp => {
+                let data = '';
 
-        resp.on('data', chunk => data += chunk);
-        resp.on('end', () => resolve(data));
-    }).on("error", reject);
-});
+                resp.on('data', chunk => (data += chunk));
+                resp.on('end', () => resolve(data));
+            })
+            .on('error', reject);
+    });
 
+/**
+ *
+ * @param {string} data
+ * @returns {Promise<Array>}
+ */
 const parseTextFile = data => {
     const lines = data.split('\n').filter(filterNonQualifiedEmoji);
 
@@ -41,7 +110,7 @@ const parseTextFile = data => {
                 currentSubGroup = {
                     name: getGroupNameFromLine(line),
                     emojis: []
-                }
+                };
             } else if (line.includes('group:')) {
                 if (currentGroup && currentSubGroup) {
                     currentGroup.subgroups.push(currentSubGroup);
@@ -66,13 +135,11 @@ const parseTextFile = data => {
                 emoji: getEmojiFromCodePoins(getEmojiCodePointFromLine(line))
             });
         }
-
-
     });
 
     return Promise.resolve(groups);
-}
+};
 
-getTextFile(URL)
+getTextFile(EMOJI_URL)
     .then(parseTextFile)
     .then(writeFile.bind(null, 'emoji-full'));
